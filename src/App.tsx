c6e7cdfad95abe5c73d7a1d1c62eb48e80cef459
zainebgan <143341107+zainebgan@users.noreply.github.com>
2025-02-13 10:30:33 +0100

    set up GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PokemonList from './components/PokemonList';
 
-
 function App() {
   return (
-    <Router>
+    <Router basename="/pokemon-app">
       <Routes>
         <Route path="/" element={<PokemonList />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
